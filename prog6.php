73998f64dc2dbe04d85d47ab5c00133c8ac6620a
Mboomikajitwta <83051104+Mboomikajitwta@users.noreply.github.com>
2021-06-23 09:44:40 +0530

    Add files via upload

---
+++ prog6.php
@@ -0,0 +1,25 @@
+<?php
+$user = "boomika";
+?>
+<!DOCTYPE html>
+<html>
+<head> <title> program 6 </title></head>
+<body>
+<h2> <?php echo $user; ?>,Welcome to Sample PHP Script </h2>
+<?php
+print "<h3> REFRESH PAGE </h3>";
+$name="counter.txt";
+$file = fopen($name,"r");
+$hits = fscanf($file,"%d");
+fclose($file);
+$hits[0]++;
+$file = fopen($name,"w");
+fprintf($file,"%d",$hits[0]);
+fclose($file);
+
+Chinmye K N
+1JT18IS018
+print "Total number of views: ".$hits[0];
+?>
+</body>
+</html>
